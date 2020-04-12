cd0ae30525a4bfe29bc2a72de410379c0f7ae8e7
Himanshu Sharma <himanshus2847@gmail.com>
2020-04-12 23:34:41 +0530

    Add files via upload

---
+++ Codes/Hand_Gesture_RX/Hand_Gesture_RX.ino
@@ -0,0 +1,72 @@
+#include <VirtualWire.h>
+#define m1 2
+#define m2 3
+#define m3 4
+#define m4 5
+#define en 6
+void setup()
+{
+    vw_set_rx_pin(11);
+    vw_setup(2000);
+    pinMode(m1, OUTPUT);
+    pinMode(m2, OUTPUT);
+    pinMode(m3, OUTPUT);
+    pinMode(m4, OUTPUT);
+    pinMode(en, OUTPUT);
+    vw_rx_start();
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN; 
+ 
+    if (vw_get_message(buf, &buflen))
+    {
+      if(buf[0]=='f')
+      {  
+       analogWrite(en, 235);
+       digitalWrite(m1,HIGH);
+       digitalWrite(m2,LOW);
+       digitalWrite(m3,HIGH);
+       digitalWrite(m4,LOW);
+       Serial.println("Forward");
+      }  
+      else if(buf[0]=='b')
+      {
+       analogWrite(en, 235);
+       digitalWrite(m1,LOW);
+       digitalWrite(m2,HIGH);
+       digitalWrite(m3,LOW);
+       digitalWrite(m4,HIGH);
+       Serial.println("Backward");
+      }
+      else if(buf[0]=='r')
+      {
+       analogWrite(en, 235);
+       digitalWrite(m1,HIGH);
+       digitalWrite(m2,LOW);
+       digitalWrite(m3,LOW);
+       digitalWrite(m4,LOW);
+       Serial.println("Left");
+      }
+      else if(buf[0]=='l')
+      {
+       analogWrite(en, 235);
+       digitalWrite(m1,LOW);
+       digitalWrite(m2,LOW);
+       digitalWrite(m3,HIGH);
+       digitalWrite(m4,LOW);
+       Serial.println("Right");
+      }
+      else if(buf[0]=='s')            
+      {
+       digitalWrite(m1,LOW);
+       digitalWrite(m2,LOW);
+       digitalWrite(m3,LOW);
+       digitalWrite(m4,LOW);
+       Serial.println("Stop");
+      }
+    }
+}
